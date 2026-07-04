062e7df723b56de71f9783ae58a0a6580b01f9c2
𝐒𝐇𝐎𝐍𝐀 𝐁𝐎𝐓𝐒🚩 <harshkumar12038@gmail.com>
2026-07-05 03:24:17 +0530

    Add TLS support for Redis connection

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -2,4 +2,9 @@ import IORedis from "ioredis";
 
 import { env } from "./env";
 
-export const redis = new IORedis(env.REDIS_URI, { maxRetriesPerRequest: null });
+const isTls = env.REDIS_URI.startsWith("rediss://");
+
+export const redis = new IORedis(env.REDIS_URI, {
+  maxRetriesPerRequest: null,
+  tls: isTls ? { rejectUnauthorized: false } : undefined,
+});
